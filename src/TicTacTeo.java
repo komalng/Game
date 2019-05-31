3fc696e54025086c074867020f77ca0840262ce1
komal.bhatt <komal18@navgurukul.org>
2019-05-31 20:24:30 +0530

    Add.Input for Row number and row index.

--- src/TicTacTeo.java
+++ src/TicTacTeo.java
@@ -4,6 +4,11 @@ public class TicTacTeo {
     public static void main(String[] args) {
         String [][] board = {{"-","-","-"},{"-","-","-"},{"-","-","-"}};
         System.out.println(printBoard(board));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Numbers");
+        int inputForRowNumber = scanner.nextInt();
+        int inputForRowIndex = scanner.nextInt();
+        System.out.println(board[inputForRowNumber][inputForRowIndex]);
     }
 
     public static String printBoard(String [][] board ){
