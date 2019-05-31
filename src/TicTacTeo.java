010f9e1d02130d99aebf06742e5aed951537c5f2
komal.bhatt <komal18@navgurukul.org>
2019-05-31 19:24:38 +0530

    Refactor.use array for making `board.

--- src/TicTacTeo.java
+++ src/TicTacTeo.java
@@ -1,17 +1,18 @@
 public class TicTacTeo {
     public static void main(String[] args) {
-        System.out.println(board());
+        String [][] board = {{"-","-","-"},{"-","-","-"},{"-","-","-"}};
+        System.out.println(board(board));
     }
 
-    public static String board(){
+    public static String board(String [][] board ){
         for(int i = 0;i < 3; i++){
             for(int a = 0; a < 3; a++){
-                System.out.print(" -"+" |");
+                System.out.print(board[i][a]);
+                System.out.print(" |");
             }
             System.out.println();
             System.out.println("-------------");
         }
         return "";
     }
-
 }
